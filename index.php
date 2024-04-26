14d3898a5accd871ca67d773c6cd57b29ab934db
Mihkel Leesment <75777801+mihkelle@users.noreply.github.com>
2024-04-26 14:27:57 +0300

    Update index.php
    background image

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
     <h2>Deployment Center</h2>
     <p>Kui ressurss on loodud, siis võtame vasakult menüüst Deployment Center. 
 Continues eelis on, et kui commit on tehtud, siis Azure automaatselt uuendab ka Azure’s ka koodi.</p>
+    <img src="Mortalis_avapilt_promo1.png" alt="Mortalis Logo image" />
   </body> 
 </html>
