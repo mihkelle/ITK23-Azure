17100e61d955d698ba21189d4ff6d3b8e5fd91c4
Mihkel Leesment <75777801+mihkelle@users.noreply.github.com>
2024-04-26 14:05:36 +0300

    Update index.php
    Lisasin põhiosa teksti, et oleks põnevam.

--- index.php
+++ index.php
@@ -4,5 +4,8 @@
   </head>
   <body>
     <h1>Meie Azure projekti pealeht</h1>
+    <h2>Deployment Center</h2>
+    <p>Kui ressurss on loodud, siis võtame vasakult menüüst Deployment Center. 
+Continues eelis on, et kui commit on tehtud, siis Azure automaatselt uuendab ka Azure’s ka koodi.</p>
   </body> 
 </html>
